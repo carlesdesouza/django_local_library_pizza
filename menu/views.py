ca4609d542d1f9113ca0b22c2d028902a6a4843b
Carles De Souza <carlesdesouza4@gmail.com>
2022-12-20 12:13:21 +0100

    First version of application moved into GitHub

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Pizza
+from django.core import serializers
 
 
 # Create your views here.
 def index(request):
     pizzas = Pizza.objects.all().order_by("price")
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by("price")
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
